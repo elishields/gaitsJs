387f9fad09bf045d2f7ca617838cd6a7f8fca12e
Eli Shields <elishields@gmail.com>
2017-05-30 11:54:53 -0700

    Further initial setup. Installed and imported react-router-dom.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <div id="global powerup">
+      <div className="App" id="global">
+          <div id="powerup">
               <p>Economy</p>
               <p>GDP</p>
           </div>
