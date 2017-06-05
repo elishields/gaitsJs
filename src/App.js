c128c3f491eb053ef416682f0fbabdb6ff35e2f1
Eli Shields <elishields@gmail.com>
2017-06-05 08:56:55 -0700

    Further work on rendering API data. Breaking callData and setData into separate components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // Import classes from React
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 
 // Import component classes
-import { Map } from './Map.js';
+import { Map } from "./Map.js";
 
 // Import styling
-import './App.css';
+import "./App.css";
 
 // Entry point for app. Loaded in index.js
 class App extends Component {
--- src/Map.js
+++ src/Map.js
@@ -1,25 +1,23 @@
 // Import classes from React
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // Import styling
-import './App.css';
+import "./App.css";
+
 
 /*
- * Renders the Map component,
- * which is a return of the MapData component.
- * Called by App component.
- * Receives no props.
- * Passes no props.
- * Stateless component.
- */
+    * Map.
+    * Renders the Map component
+    * which is a return of the MapData component.
+    * Called by App component.
+    * No props.
+    * No state.
+*/
 export class Map extends Component {
 
-    // Functionless and stateless constructor.
     constructor(props) {
         super(props);
-
         this.state = {
-
         };
     };
 
@@ -30,56 +28,73 @@ export class Map extends Component {
     };
 }
 
+
 /*
-* Renders the MapData component,
-* which is a return of a data call to Quandl's API.
+    * MapData.
+    * Renders the MapData component
+    * which is a return of the MapCallData and MapSetData
+    * components.
+    * Called by Map component.
+    * No props.
+    * No state.
 */
 class MapData extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
-            dataKey: "",
-            dataValue: ""
+        };
+    };
+
+    render() {
+        return (
+            <div>
+                <MapCallData />
+            </div>
+        )
+    };
+}
+
+/*
+    * MapCallData.
+    * Renders the MapCallData component
+    * which is a return of the callData element
+    * and the MapSetData component.
+    * Called by MapData component.
+    * Passes props to MapSetData.
+    * No state.
+*/
+class MapCallData extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
         };
 
         // Binds reference to this to member functions
+        // This binding is necessary to make `this` work in the callback
         this.callData = this.callData.bind(this);
-        this.setData = this.setData.bind(this);
     };
 
+    // Initiates a data call to Quandl's API.
     callData = function() {
         let call = new XMLHttpRequest();
         let url = "https://www.quandl.com/api/v3/datasets/FRED/GDP.json?api_key=s5ww-6M37-ytgpAy2diW&start_date=2016-01-01";
 
-        let cleanedDataKey = "";
-        let cleanedDataValue = "";
+        var cleanedDataSet = [];
 
-        call.onreadystatechange = function () {
+        call.onreadystatechange = function() {
             if (this.readyState === 4 && this.status === 200) {
                 let dataArray = JSON.parse(this.responseText);
-                cleanedDataKey = dataArray.dataset.data[0][0];
-                cleanedDataValue = dataArray.dataset.data[0][1];
-                console.log(cleanedDataKey);
-                console.log(cleanedDataValue);
-                //this.setData(cleanedDataKey, cleanedDataValue);
+                let cleanedDataKey = dataArray.dataset.data[0][0];
+                let cleanedDataValue = dataArray.dataset.data[0][1];
+                cleanedDataSet = [cleanedDataKey, cleanedDataValue];
+                console.log(cleanedDataSet);
             }
         };
 
         call.open("GET", url, true);
         call.send();
-
-        console.log(cleanedDataKey);
-        console.log(cleanedDataValue);
-    };
-
-    setData = function(dataKey, dataValue) {
-        console.log("setData called");
-        this.setState({
-            dataKey: dataKey,
-            dataValue: dataValue
-        });
     };
 
     render() {
@@ -87,9 +102,49 @@ class MapData extends Component {
             <div>
                 <input
                     type="submit"
-                    value="get data"
+                    value="get the data"
                     onClick={this.callData}
                 />
+                <MapSetData
+
+                />
+            </div>
+        )
+    };
+}
+
+/*
+    * MapSetData.
+    * Renders the MapSetData component
+    * which is a return of the setData element.
+    * Called by MapCallData component.
+    * Receives props from MapCallData.
+    * State is updated by the data returned by MapCallData.
+ */
+class MapSetData extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        };
+
+        // Binds reference to this to member functions
+        // This binding is necessary to make `this` work in the callback
+        this.setData = this.setData.bind(this);
+    };
+
+    setData(dataSet) {
+        this.setState(prevState => ({
+            dataKey: dataSet[0],
+            dataValue: dataSet[1]
+        }));
+        console.log("Setdataran");
+    };
+
+    render() {
+        return (
+            <div>
                 <p>Key:{this.state.dataKey}</p>
                 <p>Value:{this.state.dataValue}</p>
             </div>
