40e438af30f4092c480826785a24d122b4f67f27
Eli Shields <elishields@gmail.com>
2017-08-22 09:45:38 -0700

    Removed redundant callData function. DRY principle!

--- src/Grid/GridData.js
+++ src/Grid/GridData.js
@@ -23,23 +23,25 @@ export class GridData extends Component {
         super(props);
 
         this.state = {
-            usaData: 0,
-            canData: 0
+            dataValue1: 0,
+            dataValue2: 0
         };
 
         // Binds reference to "this" to member functions of GridData class
         this.callUsaData = this.callUsaData.bind(this);
-        this.callCanData = this.callCanData.bind(this);
     };
 
     // Calls JSON data from Quandl API
     // Sets the data into <GridData /> state
-    callUsaData = () => {
+    callUsaData = (whichData) => {
 
         // New object instance of XMLHttpRequest (XHR)
         let call = new XMLHttpRequest();
         // URL to access GDP data from Quandl's API
-        let url = "https://www.quandl.com/api/v3/datasets/FRED/GDPCA.json?api_key=s5ww-6M37-ytgpAy2diW&collapse=annual&start_date=2015-01-01&end_date=2017-01-01";
+        let url = [
+            "https://www.quandl.com/api/v3/datasets/FRED/GDPCA.json?api_key=s5ww-6M37-ytgpAy2diW&collapse=annual&start_date=2015-01-01&end_date=2017-01-01",
+            "https://www.quandl.com/api/v3/datasets/NBSC/A020106_A.json?api_key=s5ww-6M37-ytgpAy2diW&collapse=annual&start_date=2014-01-01&end_date=2017-01-01"
+        ];
 
         // Fires the XHR when the loading state of the page is ready
         // Parameterless fat-arrow function
@@ -53,78 +55,45 @@ export class GridData extends Component {
                 //     (key at [0], value at [1])
                 // This method of access (.dataset.data[][]) is not scalable,
                 //     as it is written specifically for Quandl's data structure
-                let data1 = dataArray.dataset.data[1][1];
-                let data2 = dataArray.dataset.data[0][1];
-                let data = (((data2 - data1) / data1) * 100 );
-                data = data.toFixed(2);
+                let dataPrevious = dataArray.dataset.data[1][1];
+                let dataCurrent = dataArray.dataset.data[0][1];
+                let dataValue = (((dataCurrent - dataPrevious) / dataPrevious) * 100 );
+                dataValue = dataValue.toFixed(2);
 
                 // Set datapoint arrays into state of <GridData />
-                this.setState({
-                    usaData: data
-                });
-                console.log("GridData state of gdp set as " + data);
+                if (whichData === 0) {
+                    this.setState({
+                        dataValue1: dataValue
+                    });
+                    console.log("GridData state of gdp set as " + dataValue);
+                }
+                if (whichData === 1) {
+                    this.setState({
+                        dataValue2: dataValue
+                    });
+                    console.log("GridData state of gdp set as " + dataValue);
+                }
             }
         };
 
         // Open XHR
-        call.open("GET", url, true);
-        // Send XHR
-        call.send();
-        console.log("XHR SENT");
-    };
-
-    // Calls JSON data from Quandl API
-    // Sets the data into <GridData /> state
-    callCanData = () => {
-
-        // New object instance of XMLHttpRequest (XHR)
-        let call = new XMLHttpRequest();
-        // URL to access GDP data from Quandl's API
-        let url = "https://www.quandl.com/api/v3/datasets/NBSC/A020106_A.json?api_key=s5ww-6M37-ytgpAy2diW&collapse=annual&start_date=2014-01-01&end_date=2017-01-01";
-
-        // Fires the XHR when the loading state of the page is ready
-        // Parameterless fat-arrow function
-        call.onreadystatechange = () => {
-            // If (XHR is finished and response is ready) and (status is ok)
-            if (call.readyState === 4 && call.status === 200) {
-                // Parse the JSON responseText from the XHR
-                let dataArray = JSON.parse(call.responseText);
-
-                // Access the most recent datapoint in the dataArray
-                //     (key at [0], value at [1])
-                // This method of access (.dataset.data[][]) is not scalable,
-                //     as it is written specifically for Quandl's data structure
-                let data1 = dataArray.dataset.data[1][1];
-                let data2 = dataArray.dataset.data[0][1];
-                let data = (((data2 - data1) / data1) * 100 );
-                data = data.toFixed(2);
-
-                // Set datapoint arrays into state of <GridData />
-                this.setState({
-                    canData: data
-                });
-                console.log("GridData state of gdp set as " + data);
-            }
-        };
-
-        // Open XHR
-        call.open("GET", url, true);
+        call.open("GET", url[whichData], true);
         // Send XHR
         call.send();
         console.log("XHR SENT");
     };
 
     componentDidMount() {
-        this.callUsaData();
-        this.callCanData();
+        this.callUsaData(0);
+        this.callUsaData(1);
         console.log("GridData mounted and callData ran");
     };
 
     render() {
         return (
             <GridLayout
-                usaData={this.state.usaData}
-                canData={this.state.canData}
+                usaData={this.state.dataValue1}
+                canData={this.state.dataValue2}
             />
         )
     };
